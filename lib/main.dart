5d7e3e2f8f31b897d8bc117b98d56f8c951b7f6e
Idam <idamachmadfaizin@gmail.com>
2020-06-14 13:30:33 +0700

    text widget & container

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,26 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(title: Text('First Flutter')),
-          body: Center(child: Text('Hello world!'))
-      ),
+          appBar: AppBar(
+            title: Text('First Flutter'),
+            backgroundColor: Colors.red,
+          ),
+          body: Center(
+              child: Container(
+                  color: Colors.lightBlue,
+                  width: 150,
+                  height: 100,
+                  child: Text(
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta quos odit voluptatum, numquam placeat omnis cum sint saepe rerum tempore, a aliquam natus, eaque magnam inventore fugiat! Non, nisi pariatur?',
+                    maxLines: 3,
+                    overflow: TextOverflow.ellipsis,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.bold),
+                  )))),
     );
   }
 }
