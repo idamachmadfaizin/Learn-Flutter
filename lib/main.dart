a4bd5c214508e25baa4ae5c5b274e026c5cf29b5
Idam <idamachmadfaizin@gmail.com>
2020-06-14 19:22:54 +0700

    stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,57 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _counter = 0;
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _dicrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(title: Text('Container')),
-          body: Container(
-            color: Colors.red,
-            margin: EdgeInsets.all(10),
-            padding: EdgeInsets.only(bottom: 20, top: 10),
-            child: Container(
-							margin: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  gradient: LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      colors: <Color>[
-                        Colors.amber,
-                        Colors.blue,
-                      ]),
-                  color: Colors.blue),
-            ),
-          )),
+        appBar: AppBar(
+          title: Text('Stateful Widget'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                '$_counter',
+                style: TextStyle(fontSize: 10 + _counter.toDouble()),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  IconButton(
+                      icon: Icon(Icons.add),
+                      onPressed: _incrementCounter,
+                      color: Colors.redAccent),
+                  RaisedButton.icon(
+                      onPressed: _dicrementCounter,
+                      icon: Icon(Icons.remove),
+                      label: Text('Remove'))
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
