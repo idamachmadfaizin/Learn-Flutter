6eec708be3393c5a396faa5bc0aba9a35b42b1b9
Idam <idamachmadfaizin@gmail.com>
2020-06-14 13:53:44 +0700

    Row And Column

--- lib/main.dart
+++ lib/main.dart
@@ -7,26 +7,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('First Flutter'),
-            backgroundColor: Colors.red,
-          ),
-          body: Center(
-              child: Container(
-                  color: Colors.lightBlue,
-                  width: 150,
-                  height: 100,
-                  child: Text(
-                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta quos odit voluptatum, numquam placeat omnis cum sint saepe rerum tempore, a aliquam natus, eaque magnam inventore fugiat! Non, nisi pariatur?',
-                    maxLines: 3,
-                    overflow: TextOverflow.ellipsis,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontStyle: FontStyle.italic,
-                        fontWeight: FontWeight.bold),
-                  )))),
+        appBar: AppBar(title: Text('Row & Col')),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text('Text 1'),
+            Text('Text 2'),
+            Text('Text 3'),
+            Row(
+              children: <Widget>[
+                Text('Text 4'),
+                Text('Text 5'),
+                Text('Text 6'),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
